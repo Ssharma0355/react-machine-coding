0c1a1e52606b19111041a6e0625b0053c8e82d29
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-25 09:14:57 +0530

    traffic light interview

--- src/traffic-light-nobroker/TrfficLight.js
+++ src/traffic-light-nobroker/TrfficLight.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./traffic.css"
 
-const TrfficLight = () => {
+const TrfficLight = ({config=[]}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(()=>{
+    if(!config.length) return
+
+    const timer = setTimeout(()=>{
+      setActiveIndex((prev)=> (prev+1) % config.length);
+    },config[activeIndex].duration)
+
+    return ()=> clearTimeout(timer)
+  },[activeIndex,config])
+ 
   return (
-    <div>
+    <div
+    style={{
+      width: "100px",
+      background: "#222",
+      padding: "20px",
+      borderRadius: "10px",
+    }}
+  >
+{config.map((light, index)=>(
+  <div key={index} 
+  style={{
+    width: "60px",
+    height: "60px",
+    margin: "10px auto",
+    borderRadius: "50%",
+    background:
+      index === activeIndex ? light.color : "#555",
+  }}>
+  </div>
+))}      
       
     </div>
   )
