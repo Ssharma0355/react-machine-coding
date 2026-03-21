8aedddd8dfe7ddb18a416e891613f0dd9a16d153
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-03-21 23:16:18 +0530

    s

--- src/debounce/Search.js
+++ src/debounce/Search.js
@@ -21,6 +21,8 @@ const Search = () => {
     },[value])
     console.table(result);
     console.table(result);
+
+    console.table(result);
     console.table(result);
     console.table(result);
 
